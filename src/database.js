187a79000c30e78f2b4e30a71a870b468459380c
Michelle Zhang <Michelle_Zhang@intuit.com>
2016-04-21 10:22:45 -0700

    adding users collection

--- src/database.js
+++ src/database.js
@@ -1,9 +1,66 @@
 import WidgetModel from './mongoose/widget';
+import UserModel from './mongoose/widget';
 import Viewer from './models/viewer';
 import User from './models/user';
 import Widget from './models/widget';
 
 export const getViewer = (id) => new Viewer({ id });
+
+/*
+		Get users from user collection
+*/
+// get user from user collection
+export const getUsersFromUser = () => {
+	return new Promise((resolve, reject) => {
+		UserModel.find({}, (err, results) => {
+			if (err) {
+				reject(err);
+				return;
+			}
+			resolve(results.map((result) => {
+				return new User(result);
+			}));
+		});
+	});
+};
+
+export const getUserFromUser = (id) => {
+	return new Promise((resolve, reject) => {
+		UserModel.findById(id, (err, result) => {
+			if (err) {
+				reject(err);
+				return;
+			}
+			resolve(result ? new User(result) : null);
+		});
+	});
+};
+
+export const updateUserFromUser = (user) => {
+	return new Promise((resolve, reject) => {
+		UserModel.findByIdAndUpdate(user._id || user.id,
+			user,
+			(err) => {
+				if (err) { reject(err); return; }
+				resolve(user ? new User(user) : null);
+			});
+	});
+};
+
+export const insertUserFromUser = (user) => {
+	return new Promise((resolve, reject) => {
+		var userModel = new UserModel(user);
+		userModel.save((err, results) => {
+			if (err) { reject(err); return; }
+			resolve(results ? new User(results) : null);
+		});
+	});
+};
+
+
+/*
+	Get users from Widget table
+*/
 
 // switch from distinct to aggregate, because distinct consider different field order to
 // be unique for the document
