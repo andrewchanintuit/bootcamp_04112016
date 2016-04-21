cc3e3394d82e6f24cd1280d77fe8e892b474b472
Mao <zihuam879@MTVL11b17773b.local>
2016-04-21 11:12:47 -0700

    graphql mutatuion file for UserFromUser CUD

---
+++ src/graphql/types/delete-user-mutation.js
@@ -0,0 +1,34 @@
+import {GraphQLID} from 'graphql';
+import {mutationWithClientMutationId, fromGlobalId} from 'graphql-relay';
+import {viewerType} from './viewer-type'
+import {getViewer, deleteUserFromUser } from '../../database';
+
+export const deleteUserMutationType = mutationWithClientMutationId({
+	//name of mutation
+	name : 'DeleteUser',
+	//input fields
+	inputFields: {
+		userId: {
+			type: GraphQLID
+		}
+	},
+
+
+	mutateAndGetPayload: ({userId}) => {
+		return deleteUserFromUser(fromGlobalId(userId).id);
+	},
+
+
+
+	outputFields: {
+		viewer: {
+			type: viewerType,
+			resolve: () =>getViewer(1)
+		},
+		userId : {
+			type: GraphQLID,
+			resolve: user =>user.id
+		}
+	}
+
+});
---
+++ src/graphql/types/update-user-mutation.js
@@ -0,0 +1,44 @@
+import { mutationWithClientMutationId, fromGlobalId, offsetToCursor } from 'graphql-relay';
+import { updateUserFromUserInputType } from './user-input-type';
+import { viewerType } from './viewer-type';
+import { UserEdge } from '../connections/user-connection';
+import { getViewer, getUsersFromUser, updateUserFromUser } from '../../database';
+
+export const updateUserMutationType = mutationWithClientMutationId({
+	// name of the mutation
+	name: 'UpdateUserShark',
+
+	inputFields: {
+		user: {
+			type: updateUserFromUserInputType
+		}
+	},
+
+	mutateAndGetPayload: ({user}) => {
+		// extract numeric widget id from global id
+		user.id = fromGlobalId(user.id).id;
+		// extract numeric owner id from global id
+		// save widget with extracted ids
+		return updateUserFromUser(user);
+	},
+
+	outputFields: {
+		viewer: {
+			type: viewerType,
+			resolve: () => getViewer(1)
+		},
+		userEdge: {
+			type: UserEdge,
+			resolve: user => {
+				return getUsersFromUser().then(users => {
+					const offset = users.indexOf(users.find(w => w.id === user.id));
+					return {
+						cursor: offsetToCursor(offset),
+						node: user
+					};
+				});
+			}
+		}
+	}
+
+});
